a7c7c72048a70f8d74a43d949b180172d8fa4ab4
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-05-07 14:40:12 +0300

    glam rock corrections more

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,10 @@
 -- This script lists all bands with Glam rock as their main style, ranked by their longevity
 -- Select the band name and calculate the lifespan in years using the split and formed attributes
-SELECT band_name, YEAR(MAX(split))-YEAR(MIN(formed)) AS lifespan
+SELECT band_name, 
+    	(CASE
+		WHEN split IS NULL THEN YEAR(CURDATE()) - formed
+		ELSE split - formed
+	END) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
-GROUP BY band_name
 ORDER BY lifespan DESC;
