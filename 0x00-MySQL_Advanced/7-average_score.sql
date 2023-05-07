f831b84f3f79677de195d23fa3fde1aa40e36f8c
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-05-07 13:07:05 +0300

    Optimize simple search

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -5,11 +5,22 @@ DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
 	DECLARE total_score FLOAT DEFAULT 0;
-    	DECLARE num_projects INT DEFAULT 0;
-	SELECT SUM(score), COUNT(*) INTO total_score, num_projects FROM corrections WHERE user_id = user_id;
-	IF num_projects > 0 THEN
-		UPDATE users SET average_score = total_score / num_projects WHERE id = user_id;
+    	DECLARE num_scores INT DEFAULT 0;
+	DECLARE avg_score FLOAT DEFAULT 0;
+
+	SELECT SUM(score), COUNT(*)
+	INTO total_score, num_scores
+	FROM corrections
+	WHERE user_id = user_id;
+
+	IF num_scores > 0 THEN
+		SET avg_score = total_score / num_scores;
 	END IF;
+
+	UPDATE users
+	SET average_score = avg_score
+	WHERE id = user_id;
+
 END //
 
 DELIMITER ;
