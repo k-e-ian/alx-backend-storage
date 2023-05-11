915e0148a8a86ef1da509b2e0a0d9f8f77599b62
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-05-11 05:19:37 +0300

    reading from redis and recovering original type'

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,15 +4,12 @@ File: exercise.py
 '''
 
 
-import redis
+        return key
+rt redis
 import uuid
-from typing import Union
-
+from typing import Callable, Union
 
 class Cache:
-    '''
-    store instance of Redis cli as priv var named redis, flush instance flushdb
-    '''
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
@@ -21,3 +18,20 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+        if not self._redis.exists(key):
+            return None
+
+        data = self._redis.get(key)
+
+        if fn is not None:
+            data = fn(data)
+
+        return data
+
+    def get_str(self, key: str) -> Union[str, None]:
+        return self.get(key, fn=lambda d: d.decode('utf-8') if d is not None else None)
+
+    def get_int(self, key: str) -> Union[int, None]:
+        return self.get(key, fn=int) if self.get(key) is not None else None
