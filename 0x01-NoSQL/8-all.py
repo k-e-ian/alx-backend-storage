08fc2212e7e291b389cbd291a082a3bc3611cf53
Ian Edwin Kitembe <ianedwin@outlook.com>
2023-05-13 15:13:50 +0300

    pymongo

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''
+File: 8-all.py
+'''
+
+def list_all(mongo_collection):
+    '''
+    function that lists all documents in a collection
+    '''
+    return list(mongo_collection.find())
